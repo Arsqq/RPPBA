70b02d64aab6bc0b15564246d1a84867baa91a6a
Arsqq <arny723@gmail.com>
2022-04-25 16:31:05 +0300

    component

---
+++ src/main/java/com/example/rppbaproject/Controllers/DungeonMaster.java
@@ -0,0 +1,7 @@
+package com.example.rppbaproject.Controllers;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DungeonMaster {
+}
---
+++ src/main/java/com/example/rppbaproject/DungeonMaster.java
@@ -0,0 +1,4 @@
+package com.example.rppbaproject;
+
+public class DungeonMaster {
+}
