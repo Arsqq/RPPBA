abdc70bb73c84ec9abd57e5d8fe208f04c6ae013
Arsqq <arny723@gmail.com>
2022-04-25 16:30:49 +0300

    component

--- src/main/java/com/example/rppbaproject/Repo/UserRepo.java
+++ src/main/java/com/example/rppbaproject/Repo/UserRepo.java
@@ -2,7 +2,9 @@ package com.example.rppbaproject.Repo;
 
 import com.example.rppbaproject.Domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository <User,Long> {
     boolean existsByLoginAndPassword(String login, String password);
 }
